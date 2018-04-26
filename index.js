88041d3a8845863c0e2fa48049a75a6b7d2629e8
Khojiakbar Karimov <kh.karimov96@gmail.com>
2018-04-26 09:46:54 -0700

    EVOS food initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'EVOs Enjoy Fast Food in any time',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu.hbs', {
+      pageTitle: 'EVOs Menu'
+    });
+  });
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
